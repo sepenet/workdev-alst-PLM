cc6fb8abd551edec7f1dffec43d8037a09a3690c
Sebastien Penet <sepenet@microsoft.com>
2023-08-08 09:28:10 +0200

    build csv file out of diskspeed test initial commit

---
+++ diskSpdTest/buildData.ps1
@@ -0,0 +1,5 @@
+.\createBaseFile.ps1
+.\createFile.ps1 .\dataSource\ephemeral1 "ephemeral"
+.\createFile.ps1 .\dataSource\ephemeral2 "ephemeral"
+.\createFile.ps1 .\dataSource\ephemeral3 "ephemeral"
+.\createFile.ps1 .\dataSource\premiumssd1 "Premium SSD"
